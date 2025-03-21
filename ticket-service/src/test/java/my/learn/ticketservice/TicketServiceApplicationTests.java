8916636ee5ad91ec47b091a5ec15668b0668eeb4
Anton_Markin <anton.markin1008@yandex.ru>
2025-03-21 12:56:52 +0300

    Ticket-service added

---
+++ ticket-service/src/test/java/my/learn/ticketservice/TicketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package my.learn.ticketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
