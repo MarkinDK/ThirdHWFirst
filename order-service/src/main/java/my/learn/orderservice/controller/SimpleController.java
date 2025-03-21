f03960152a61c44ca050c8b6dbffb27a5c375b5c
Anton_Markin <anton.markin1008@yandex.ru>
2025-03-21 11:44:29 +0300

    Config server uses local configs depending on profile, runtime properties changes added to order-service (to update property make POST request on /actuator/refresh)

--- order-service/src/main/java/my/learn/orderservice/controller/SimpleController.java
+++ order-service/src/main/java/my/learn/orderservice/controller/SimpleController.java
@@ -1,15 +1,19 @@
 package my.learn.orderservice.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 @RequestMapping("/order")
 public class SimpleController {
+
     @GetMapping("/test")
-    public String test() {
-        return "test";
+    public String test(@Value("${property}") String property) {
+        return property;
     }
 }
