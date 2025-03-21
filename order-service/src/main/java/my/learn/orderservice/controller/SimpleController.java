4ac0033c230a1f9478c4d289c59f7f451938b3b5
Anton_Markin <anton.markin1008@yandex.ru>
2025-03-21 09:43:40 +0300

    Controller created and works via api gateway.

---
+++ order-service/src/main/java/my/learn/orderservice/controller/SimpleController.java
@@ -0,0 +1,15 @@
+package my.learn.orderservice.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/simple")
+public class SimpleController {
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
