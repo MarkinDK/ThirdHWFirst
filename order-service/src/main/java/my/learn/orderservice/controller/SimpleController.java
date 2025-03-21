e49bce2b3b3c731bc041f7d5ebb951b5eb5e5197
Anton_Markin <anton.markin1008@yandex.ru>
2025-03-21 10:47:21 +0300

    SimpleController created in payment-service, jpa commented in pom

--- order-service/src/main/java/my/learn/orderservice/controller/SimpleController.java
+++ order-service/src/main/java/my/learn/orderservice/controller/SimpleController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/simple")
+@RequestMapping("/order")
 public class SimpleController {
     @GetMapping("/test")
     public String test() {
