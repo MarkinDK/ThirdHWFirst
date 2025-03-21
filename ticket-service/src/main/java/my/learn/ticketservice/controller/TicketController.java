63beb42931ec65796eba9d1ab6ce833aa1550f5f
Anton_Markin <anton.markin1008@yandex.ru>
2025-03-21 13:37:48 +0300

    Database added to ticket-service and repository and service created

--- ticket-service/src/main/java/my/learn/ticketservice/controller/TicketController.java
+++ ticket-service/src/main/java/my/learn/ticketservice/controller/TicketController.java
@@ -1,22 +1,22 @@
 package my.learn.ticketservice.controller;
 
-import my.learn.ticketservice.model.Ticket;
+import my.learn.ticketservice.service.TicketService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.UUID;
-
 @RestController
 @RequestMapping("/tickets")
 public class TicketController {
+    private final TicketService ticketService;
+
+    public TicketController(TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+
     @GetMapping
     public ResponseEntity<?> getAllTickets() {
-        return ResponseEntity.ok().body(List.of(
-                new Ticket(UUID.randomUUID(), UUID.randomUUID(), UUID.randomUUID()),
-                new Ticket(UUID.randomUUID(), UUID.randomUUID(), UUID.randomUUID()),
-                new Ticket(UUID.randomUUID(), UUID.randomUUID(), UUID.randomUUID())));
+        return ResponseEntity.ok().body(ticketService.findAll());
     }
 }
--- ticket-service/src/main/java/my/learn/ticketservice/model/Ticket.java
+++ ticket-service/src/main/java/my/learn/ticketservice/model/Ticket.java
@@ -1,16 +1,23 @@
 package my.learn.ticketservice.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Ticket {
+    @Id
     private UUID ticketId;
+    @Column(unique = true, nullable = false)
     private UUID orderId;
+    @Column(unique = true, nullable = false)
     private UUID paymentId;
 }
