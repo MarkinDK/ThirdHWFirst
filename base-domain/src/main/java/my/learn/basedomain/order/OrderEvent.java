c0744c9e508e860470f047da315be2a8c9be25ba
Anton_Markin <anton.markin1008@yandex.ru>
2025-03-25 13:46:59 +0300

    Controller created to test Kafka connection

--- base-domain/src/main/java/my/learn/basedomain/order/OrderEvent.java
+++ base-domain/src/main/java/my/learn/basedomain/order/OrderEvent.java
@@ -1,6 +1,12 @@
 package my.learn.basedomain.order;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderEvent {
     private OrderInfo order;
     private OrderStatus status;
--- order-service/src/main/java/my/learn/orderservice/controller/OrderController.java
+++ order-service/src/main/java/my/learn/orderservice/controller/OrderController.java
@@ -1,23 +1,43 @@
 package my.learn.orderservice.controller;
 
+import my.learn.basedomain.order.OrderEvent;
+import my.learn.basedomain.order.OrderInfo;
+import my.learn.basedomain.order.OrderStatus;
+import my.learn.orderservice.kafka.OrderProducer;
 import my.learn.orderservice.service.OrderService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
     private final OrderService orderService;
+    private final OrderProducer orderProducer;
 
-    public OrderController(OrderService orderService) {
+    public OrderController(OrderService orderService, OrderProducer orderProducer) {
         this.orderService = orderService;
+        this.orderProducer = orderProducer;
     }
 
     @GetMapping
     public ResponseEntity<?> getAllOrders() {
         return ResponseEntity.status(HttpStatus.OK).body(orderService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createOrder() {
+        orderProducer.sendOrderEvent(
+                new OrderEvent(
+                        new OrderInfo(UUID.randomUUID(), 1000),
+                        OrderStatus.CREATED,
+                        "Some order created in kafka"
+                ));
+        return ResponseEntity.ok().build();
     }
 }
